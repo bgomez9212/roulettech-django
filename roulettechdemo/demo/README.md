03f8e8526310fec05ffba6642659ee2531bc792f
Brandon Gomez <105175292+bgomez9212@users.noreply.github.com>
2024-07-13 21:47:53 -0700

    Delete roulettechdemo directory

--- roulettechdemo/demo/README.md
+++
@@ -1,1 +0,0 @@
-# roulettech-demo
--- roulettechdemo/demo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ArticleList, ArticleDetail, CommentList, CommentDetail
-
-urlpatterns = [
-  path('articles/', ArticleList.as_view()),
-  path('articles/<int:pk>', ArticleDetail.as_view()),
-  path('comments/', CommentList.as_view()),
-  path('comments/<int:pk>', CommentDetail.as_view())
-]
