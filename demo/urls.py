136f8f0afff289dfd7153981e80e1e4185d899a1
Brandon Gomez <bgomez9212@gmail.com>
2024-07-16 12:59:17 -0700

    update views and urls to handle getting comments by article id

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ArticleList, ArticleDetail, CommentList, CommentDetail
+from .views import ArticleList, ArticleDetail, CommentList, CommentDetail, ArticleComments
 
 urlpatterns = [
   path('articles/', ArticleList.as_view()),
   path('articles/<int:pk>', ArticleDetail.as_view()),
   path('comments/', CommentList.as_view()),
-  path('comments/<int:pk>', CommentDetail.as_view())
-]
+  path('comments/<int:pk>', CommentDetail.as_view()),
+  path('articles/<int:pk>/comments', ArticleComments.as_view())
+  ]
