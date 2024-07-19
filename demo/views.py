0cab5f417b2b5e97c682c6ceba8b1dc7b9184716
Brandon Gomez <bgomez9212@gmail.com>
2024-07-19 12:54:02 -0700

    another fix

--- demo/views.py
+++ demo/views.py
@@ -7,7 +7,7 @@ class CommentList(generics.ListCreateAPIView):
 
   def get_queryset(self):
     querySet = Comments.objects.all()
-    article_id = self.kwargs.get('pk', None)
+    article_id = self.kwargs['pk']
     if article_id is not None:
       querySet = querySet.filter(article=article_id)
     return querySet
